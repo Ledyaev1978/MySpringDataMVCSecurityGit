acc81adc1c0d170a4f24111a0899b5db21d4247f
Ledyaev1978 <chess_home@bk.ru>
2023-08-12 13:09:58 +0300

    First commit.Template spring.io .

---
+++ src/main/java/by/project/myspringdatamvcsecuritygit/MySpringDataMvcSecurityGitApplication.java
@@ -0,0 +1,13 @@
+package by.project.myspringdatamvcsecuritygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringDataMvcSecurityGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringDataMvcSecurityGitApplication.class, args);
+    }
+
+}
